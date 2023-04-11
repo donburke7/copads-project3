85208207a1df95f83d5c08d0cece5514f5097743
donburke7 <donburke7@gmail.com>
2023-04-11 00:33:15 -0400

    Started GetKey functionality

---
+++ GetKey.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GetKeyApplication 
+{
+
+    public class GetKey {
+        public static readonly HttpClient client;
+
+        public static void Main(String[] args) {
+            string testEmail = "toivcs@rit.edu";
+
+            var KeyGet = new GetKey();
+            KeyGet.getKeyClient(testEmail);
+        }
+
+        public async void getKeyClient(string email) {
+            string responseBody = await client.GetStringAsync("http://kayrun.cs.rit.edu:5000/Message/" + email);
+
+            Console.WriteLine(responseBody);
+        }
+    }
+}
