14c34c96addb31a325336a192e9b8f72bf9862fe
donburke7 <donburke7@gmail.com>
2023-04-18 23:58:15 -0400

    Fixed keyGen infinite run.

--- Program.cs
+++ Program.cs
@@ -15,11 +15,11 @@ namespace Messenger {
                 var keyClient = new KeyClient(args[1]);
                 var msgClient = new MsgClient(args[1]);
                 if (args[0].Equals("keyGen")) {
-                    if (Int32.TryParse(args[1], out int keySize) && keySize > 0) {
+                    if (Int32.TryParse(args[1], out int keySize) && keySize >= 32) {
                         KeyGenerator keyGen = new KeyGenerator(keySize);
                         keyGen.genKeys();
                     }
-                    else { Console.Out.WriteLine(args[1] + " must be a positive whole number."); }
+                    else { Console.Out.WriteLine(args[1] + " must be a positive whole number >= 32."); }
                 }
                 else if (args[0].Equals("getKey")) {
                     keyClient.getKey();
